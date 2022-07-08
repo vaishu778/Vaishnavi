e8cc0b48c983f2acaaddc606c22aeb1178cdda79
vaishu778 <108948875+vaishu778@users.noreply.github.com>
2022-07-08 22:33:29 +0530

    Add files via upload

---
+++ craftBeer.py
@@ -0,0 +1,10 @@
+
+data = {
+    'brewery_id': [10325, 10326],
+    'brewery_name': ['Vecchio Birralo', 'Vecchio Birraio'],
+    'beer_style': ['Hefeweizer', 'English Strong Ale'],
+    'beer_name': ['Sausa Weizer', 'Red Moon']
+}
+print("Thippeswamy Chandra Shekhar pulipati\n1MJ19CS179")
+for key, value in data.items():
+    print(key, ':', value)
